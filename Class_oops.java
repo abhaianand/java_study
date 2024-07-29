7eb1de93f21b7775a90ebc5da8d02ea94631d75a
abhai <103772609+abhaianand@users.noreply.github.com>
2024-07-29 18:08:34 +0530

    test

--- Class_oops.java
+++ Class_oops.java
@@ -1,4 +1,15 @@
 package java_study;
 
 public class Class_oops {
+    public static void main (String[] args){
+
+        // class - collection of attributes and behavior or blue print  (attribute - variables , behavior - methods/ functions) non physical - does not require any memory
+        //object - instance of a class ,physical - require the memory
+        //can create multiple   object  of a single class
+        //syntax -  type of variable  variable = new class name ();   type of variable mostly same class name
+        System.out.println("h");
+
+
+
+    }
 }
--- Oops.java
+++ Oops.java
@@ -1,2 +1,16 @@
-package java_study;public class Oops {
+package java_study;
+
+public class Oops {
+
+    public static void main (String[] args){
+
+        //class
+        //object
+        //polymorphism
+        //encapsulation
+        //inheritance
+        //abstraction
+        System.out.println("h");
+
+    }
 }
