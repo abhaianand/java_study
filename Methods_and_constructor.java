b00909937a2084997910b8303e7f1d984f030dbf
abhaianand <abhaianand6@gmail.com>
2024-07-30 21:39:59 +0530

    dfd
    dfdf

---
+++ Methods_and_constructor.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class Methods_and_constructor {
+
+ //methods
+ // group of statements that perform certain task.
+    //we have to call methode though object.
+
+
+
+
+}
