37eb26171da773cdb21fccda363c8bc4a06db02a
abhai <103772609+abhaianand@users.noreply.github.com>
2024-08-01 17:46:03 +0530

    ada
    ada

---
+++ polymorphism_encapsulation_overloading/Poly.java
@@ -0,0 +1,27 @@
+package java_study.polymorphism_encapsulation_overloading;
+
+public class Poly {
+    // method overloading
+    // 1) method name should be same
+    //2) number of parameter should be diffrernt
+    //3) data type should be different
+    //4) oreder of parameter should be different
+    // 5) variable name not consider as a change
+    int a = 100;
+    int b =2;
+    void overloading (){
+        System.out.println(a+b); //1
+    }
+    void overloading(int x,int y){
+        System.out.println(x+y); //2
+    }
+    void overloading(int x,double y){
+        System.out.println(x+y); //3
+    }
+    void overloading(double x,int y){
+        System.out.println(x+y); //4
+    }
+    void overloading(int x,double y,int z){
+        System.out.println(x+y+z); //5
+    }
+}
---
+++ polymorphism_encapsulation_overloading/Poly_main.java
@@ -0,0 +1,14 @@
+package java_study.polymorphism_encapsulation_overloading;
+
+public class Poly_main {
+    public static void main(String[] args){
+
+
+        Poly obj =  new Poly();
+        obj.overloading(); //1
+        obj.overloading(10,12); //2
+        obj.overloading(20,10.5); //3
+        obj.overloading(11.5,30); //4
+        obj.overloading(11,30.3,12); //5
+    }
+}
